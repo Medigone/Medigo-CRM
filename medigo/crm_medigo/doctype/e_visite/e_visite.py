3eefa7b900d3a7a5825df3b32a21a71420191f83
Medigone <145498756+Medigone@users.noreply.github.com>
2024-03-05 22:24:36 +0100

    Update e_visite.py

--- medigo/crm_medigo/doctype/e_visite/e_visite.py
+++ medigo/crm_medigo/doctype/e_visite/e_visite.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2024, Amine Melizi and contributors
 # For license information, please see license.txt
 
-# import frappe
-from frappe.website.website_generator import WebsiteGenerator
+import frappe
 
+from frappe.website.website_generator import WebsiteGenerator
+from frappe.model.document import Document
 
 class EVisite(Document):
 	pass
